85abd9f02b33bef98b48d2639ce7412ebcf179c4
angerding <angerding@gmail.com>
2024-06-08 09:48:04 -0500

    creación de app "apis" y creación de api en framework

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3er party
+    'rest_framework', #new
     #local
     'books.apps.BooksConfig',
+    'apis.apps.ApisConfig',
 ]
 
 MIDDLEWARE = [
